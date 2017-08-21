f313c3bcea2ea52cc33dee81c79b5f4384072d33
Hope Ranker <hope.ranker@gmail.com>
2017-08-21 03:13:04 -0400

    use guard for simpleMatch

--- src/Fazzbozz.hs
+++ src/Fazzbozz.hs
@@ -1,13 +1,12 @@
 module Fazzbozz (fazzbozz, simpleMatch) where
 
+import Control.Monad
 import Data.Maybe
 
 type Match = Int -> Maybe String
 
 simpleMatch :: Int -> String -> Match
-simpleMatch count label n
-  | n `mod` count == 0 = Just label
-  | otherwise = Nothing
+simpleMatch count label n = label <$ guard (n `mod` count == 0)
 
 fazzbozz :: [Match] -> Int -> String
 fazzbozz matches = fromMaybe <$> show <*> mconcat matches
