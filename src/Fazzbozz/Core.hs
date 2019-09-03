a4ef731d22ac05bcd502c560982cb79132f81557
Hope Ranker <hope.ranker@gmail.com>
2019-09-03 16:57:58 -0400

    twiddle fazzWithLabel into fazzOne

--- src/Fazzbozz/Core.hs
+++ src/Fazzbozz/Core.hs
@@ -6,23 +6,30 @@ module Fazzbozz.Core (
 
 import Control.Monad
 import Data.Maybe
+import Data.Tuple (swap)
 
 import Fazzbozz.Base
 
 type Label = String
 data Labeled s = Labeled s Label deriving (Eq, Show)
 
+instance Functor Labeled where
+  fmap f (Labeled s l) = Labeled (f s) l
+
 sfazzbozz :: FazzState s => [Labeled s] -> Integer -> (String, [Labeled s])
 sfazzbozz ss n = mapFst collectResults $ unzip $ fazzAll ss
   where
     collectResults = fromMaybe (show n) . mconcat
-    fazzAll = map $ fazzWithLabel n
+    fazzAll = map $ fazzOne n
 
-fazzWithLabel :: FazzState s => Integer -> Labeled s -> (Maybe String, Labeled s)
-fazzWithLabel n (Labeled s label) = (result, Labeled s' label)
-  where
-    (s', result) = mapSnd labelWhen $ matchFazz s n
-    labelWhen maybeMatch = label <$ guard maybeMatch
+fazzOne :: FazzState s => Integer -> Labeled s -> (Maybe String, Labeled s)
+fazzOne n = swap . fmap labelToMaybe . dupLabel . fmap (flip matchFazz $ n)
+
+dupLabel :: Labeled (a, b) -> (Labeled a, Labeled b)
+dupLabel (Labeled (a, b) lab) = (Labeled a lab, Labeled b lab)
+
+labelToMaybe :: Labeled Bool -> Maybe String
+labelToMaybe (Labeled b label) = label <$ guard b
 
 scanM :: Foldable t => (a -> b -> (c, a)) -> a -> t b -> [c]
 scanM f s ns = fst $ foldM f' s ns
