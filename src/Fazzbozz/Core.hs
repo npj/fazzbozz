f033306df1ebc3343c6e259d8512ac6245bd69bb
Hope Ranker <hope.ranker@gmail.com>
2019-09-03 23:04:37 -0400

    define Labeled after all
    I know, I just removed it. It wasn't strictly necessary as a data def.
    But as a type it makes things a tiny bit clearer.

--- src/Fazzbozz/Core.hs
+++ src/Fazzbozz/Core.hs
@@ -10,20 +10,21 @@ import Data.Tuple (swap)
 import Fazzbozz.Base
 
 type Label = String
+type Labeled s = (Label, s)
 
-sfazzbozz :: FazzState s => [(Label, s)] -> Integer -> (String, [(Label, s)])
+sfazzbozz :: FazzState s => [Labeled s] -> Integer -> (String, [Labeled s])
 sfazzbozz ss n = mapFst collectResults $ unzip $ fazzAll ss
   where
     collectResults = fromMaybe (show n) . mconcat
     fazzAll = map $ fazzOne n
 
-fazzOne :: FazzState s => Integer -> (Label, s) -> (Maybe String, (Label, s))
+fazzOne :: FazzState s => Integer -> Labeled s -> (Maybe String, Labeled s)
 fazzOne n = mapFst labelToMaybe . dupFst . fmap (flip matchFazz $ n)
 
 dupFst :: (a, (b, c)) -> ((a, b), (a, c))
 dupFst (a, (b, c)) = ((a, b), (a, c))
 
-labelToMaybe :: (Label, Bool) -> Maybe String
+labelToMaybe :: Labeled Bool -> Maybe String
 labelToMaybe (label, b) = label <$ guard b
 
 scanM :: Foldable t => (a -> b -> (c, a)) -> a -> t b -> [c]
