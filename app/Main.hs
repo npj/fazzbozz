c9b061c9f0b58c2c326c4b00df544d6eaef866cd
Peter Brindisi <pbrindisi@pandora.com>
2019-11-01 16:00:20 -0400

    definitions and examples

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 import Data.Maybe
 import Options.Applicative
-import Text.Read
 
 import Fazzbozz
 import Fazzbozz.CmdOptions 
@@ -31,14 +30,60 @@ addStuff' = do
 
 data State s a = State (s -> (a, s))
 
+data MyEnv = MyEnv {
+    intValue :: Int
+  , strValue :: String
+} deriving (Show)
+
+myStatefulComp :: Int -> State MyEnv Int
+myStatefulComp x = do
+  env <- get
+  set $ env { intValue = (intValue env) + x }
+  return $ x + 10
+
+myOtherStatefulComp :: Int -> State MyEnv Int
+myOtherStatefulComp x = do
+  env <- get
+  set $ env { intValue = (intValue env) + x }
+  return $ x + 20
+
+combined :: State MyEnv Int
+combined = do
+  myStatefulComp 5 >>= \int1 ->
+    myStatefulComp 10 >>= \int2 ->
+      return $ int1 + int2
+
+doTheThings :: MyEnv -> (Int, MyEnv)
+doTheThings env = runState combined env
+
+-- > doTheThings (MyEnv { intValue = 0, strValue = "" })
+--   (35,MyEnv { intValue = 15, strValue = "" })
+
+instance Functor (State s) where
+  fmap = undefined
+
+instance Applicative (State s) where
+  pure = undefined
+  (<*>) = undefined
+
 instance Monad (State s) where
   return x = State $ \s -> (x, s)
-  (State sf) >>= f = State $ \s -> let (a, s') = sf s
-                                       (State sg) = f a
-                                       in sg s'
+  initComp >>= f = State $ \initEnv ->
+    let (value, newEnv) = runState initComp initEnv
+        newComp = f value
+        in runState newComp newEnv
+                                     
+runState :: State s a -> s -> (a, s)
+runState (State f) s = f s
 
--- execState :: State s a -> s -> a
--- evalState :: State s a -> s -> s
+get :: State s s
+get = State $ \s -> (s, s)
+
+set :: s -> State s ()
+set newState = State $ \_ -> ((), newState)
+
+-- execState :: State s a -> s -> s
+-- evalState :: State s a -> s -> a
 -- runState :: State s a -> s -> (a, s)
 
 main = execParser opts >>= printFazzbozz
